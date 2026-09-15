abea62ca9cda45276985da5c37f043c1982e1ee4
kartikparashar9 <parasharkartik480@gmail.com>
2026-09-15 18:03:39 +0530

    Backend fix

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -26,12 +26,18 @@ const isEmailConfigured = () => {
 // =====================================================
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
 
   auth: {
     user: SMTP_USER,
     pass: SMTP_PASSWORD,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 // =====================================================
